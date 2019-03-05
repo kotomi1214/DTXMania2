7d0cf5e16cb3998419813b76c6b93f968dc763a1
ＦＲＯＭ <kumakamikoubou@gmail.com>
2019-03-06 03:03:20 +0900

    FDK.カウンタ.QueueTimerクラスを追加。

--- SSTFEditor/Properties/AssemblyInfo.cs
+++ SSTFEditor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "SSTFEditor" )]
-[assembly: AssemblyCopyright( "Copyright © 2009 2018 ＦＲＯＭ／くまかみ工房" )]
+[assembly: AssemblyCopyright( "Copyright © 2009 2019 ＦＲＯＭ／くまかみ工房" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
