8bcabb88fafc215bbfdf4a3cfaa4dbf8d0abff39
ＦＲＯＭ <kumakamikoubou@gmail.com>
2019-07-14 21:10:15 +0900

    バージョンを更新。

--- DTXMania/Properties/AssemblyInfo.cs
+++ DTXMania/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "42.0.0.0" )]
-[assembly: AssemblyFileVersion( "42.0.0.0" )]
+[assembly: AssemblyVersion( "43.0.0.0" )]
+[assembly: AssemblyFileVersion( "43.0.0.0" )]
