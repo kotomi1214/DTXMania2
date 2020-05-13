724bdab3013d9668a03531a1ca387cc6a79dff4a
FROM / Kenji Yamazaki <kumakamikoubou@gmail.com>
2020-05-13 14:56:26 +0900

    SSTFEditorプロジェクト一式を追加。ただしDTXMania連携はまだ。

---
+++ FDK/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FDK
+{
+    /// <summary>
+    ///     System.String の拡張メソッド
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        ///		文字列が Null でも空でもないなら true を返す。
+        /// </summary>
+        public static bool Nullでも空でもない( this string 検査対象 )
+            => !( string.IsNullOrEmpty( 検査対象 ) );
+
+        /// <summary>
+        ///		文字列が Null または空なら true を返す。
+        /// </summary>
+        public static bool Nullまたは空である( this string 検査対象 )
+            => string.IsNullOrEmpty( 検査対象 );
+    }
+}
---
+++ SSTFEditor/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSTFEditor
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode( HighDpiMode.SystemAware );
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault( false );
+
+            try
+            {
+                Application.Run( new メインフォーム() );
+            }
+#if !DEBUG
+            catch( Exception e )
+            {
+                //using( var dlg = new 未処理例外検出ダイアログ() )
+                {
+                    Trace.WriteLine( "" );
+                    Trace.WriteLine( "====> 未処理の例外が検出されました。" );
+                    Trace.WriteLine( "" );
+                    Trace.WriteLine( e.ToString() );
+
+                    //dlg.ShowDialog();
+                }
+            }
+#else
+            finally
+            {
+                // DEBUG 時には、未処理の例外が発出されてもcatchしない。（デバッガでキャッチすることを想定。）
+            }
+#endif
+        }
+    }
+}
