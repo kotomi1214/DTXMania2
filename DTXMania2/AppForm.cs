ecbbefe68340e6f1d56bd4273ec837db55090794
FROM / Kenji Yamazaki <kumakamikoubou@gmail.com>
2020-06-04 17:32:06 +0900

    Program.Main() を実装。

--- DTXMania2/AppForm.cs
+++ DTXMania2/AppForm.cs
@@ -4,18 +4,134 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FDK;
 
 namespace DTXMania2
 {
     public partial class AppForm : Form
     {
+
+        // プロパティ
+
+
+        /// <summary>
+        ///     アプリケーション再起動指示フラグ。
+        /// </summary>
+        /// <remarks>
+        ///     <see cref="AppForm"/> インスタンスの終了時にこのフラグが true になっている場合には、
+        ///     このインスタンスの保持者（おそらくProgramクラス）は適切に再起動を行うこと。
+        /// </remarks>
+        public bool 再起動が必要 { get; protected set; } = false;
+
+
+
+        // 生成と終了
+
+
+        /// <summary>
+        ///     コンストラクタ。
+        /// </summary>
         public AppForm()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        ///     アプリケーションの起動処理を行う。
+        /// </summary>
+        protected override void OnLoad( EventArgs e )
+        {
+            Log.Header( "アプリケーション起動" );
+            using var _ = new LogBlock( Log.現在のメソッド名 );
+
+            // フォームを設定する。
+            this.Text = $"DTXMania2 Release {int.Parse( Application.ProductVersion.Split( '.' ).ElementAt( 0 ) ):000}";
+            this.ClientSize = new Size( 1024, 576 );
+            this.Icon = Properties.Resources.DTXMania2;
+            this._ScreenMode = new ScreenMode( this );
+
+            // 入力デバイスを初期化する。
+            this._KeyboardHID = new KeyboardHID();
+            this._GameControllersHID = new GameControllersHID( this.Handle );
+            this._MidiIns = new MidiIns();
+
+            // 初期化完了。
+            this._未初期化 = false;
+
+            base.OnLoad( e );
+        }
+
+        /// <summary>
+        ///     アプリケーションの終了処理を行う。
+        /// </summary>
+        protected override void OnClosing( CancelEventArgs e )
+        {
+            Log.Header( "アプリケーション終了" );
+            using var _ = new LogBlock( Log.現在のメソッド名 );
+
+            // 入力デバイスを破棄する。
+            this._MidiIns.Dispose();
+            this._GameControllersHID.Dispose();
+            this._KeyboardHID.Dispose();
+
+            // 未初期化状態へ。
+            this._未初期化 = true;
+
+            base.OnClosing( e );
+        }
+
+        /// <summary>
+        ///     再起動フラグをセットして、アプリケーションを終了する。
+        /// </summary>
+        public void 再起動する()
+        {
+            using var _ = new LogBlock( Log.現在のメソッド名 );
+
+            this.再起動が必要 = true;
+            this.Close();
+        }
+
+
+
+        // ローカル
+
+
+        /// <summary>
+        ///     アプリの初期化が完了していなければ true。
+        ///     起動直後は true, OnLoad() で false, OnClosing() で true になる。
+        /// </summary>
+        private bool _未初期化 = true;
+
+        /// <summary>
+        ///     画面モード（ウィンドウ、全画面）。
+        /// </summary>
+        private ScreenMode _ScreenMode = null!;
+
+        /// <summary>
+        ///     HIDキーボード入力。
+        /// </summary>
+        /// <remarks>
+        ///     接続されているHIDキーボードを管理する。
+        ///     RawInputを使うので、フォームのスレッドで管理する。（RawInputはUIスレッドでのみ動作する）
+        /// </remarks>
+        private KeyboardHID _KeyboardHID = null!;
+
+        /// <summary>
+        ///     すべてのHIDゲームパッド、HIDジョイスティック入力。
+        /// </summary>
+        /// <remarks>
+        ///     すべてのゲームパッド／ジョイスティックデバイスを管理する（<see cref="GameControllersHID.Devices"/>参照）。
+        ///     RawInputを使うので、フォームのスレッドで管理する。（RawInputはUIスレッドでのみ動作する）
+        /// </remarks>
+        private GameControllersHID _GameControllersHID = null!;
+
+        /// <summary>
+        ///     すべてのMIDI入力デバイス。
+        /// </summary>
+        private MidiIns _MidiIns = null!;
     }
 }
---
+++ DTXMania2/Global.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using FDK;
+
+namespace DTXMania2
+{
+    /// <summary>
+    ///     グローバルリソース。すべて static。
+    /// </summary>
+    static class Global
+    {
+
+        // グローバルプロパティ
+
+
+        /// <summary>
+        ///     メインフォームインスタンスへの参照。
+        /// </summary>
+        public static AppForm AppForm { get; set; } = null!;
+
+        /// <summary>
+        ///     メインフォームインスタンスのウィンドウハンドル。
+        /// </summary>
+        /// <remarks>
+        ///     <see cref="AppForm.Handle"/> と同じ値であるが、GUIスレッド 以 外 のスレッドから参照する場合は、
+        ///     <see cref="AppForm.Handle"/> ではなくこのメンバを参照すること。
+        ///     （<see cref="DTXMania2.AppForm"/> のメンバはすべて、必ずGUIスレッドから参照されなれければならないため。）
+        /// </remarks>
+        public static IntPtr Handle { get; set; } = IntPtr.Zero;
+
+        /// <summary>
+        ///     アプリ起動時のコマンドラインオプション。
+        /// </summary>
+        public static CommandLineOptions Options { get; set; } = null!;
+
+        /// <summary>
+        ///     設計（プログラム側）で想定する固定画面サイズ[dpx]。
+        /// </summary>
+        /// <remarks>
+        ///     物理画面サイズはユーザが自由に変更できるが、プログラム側では常に設計画面サイズを使うことで、
+        ///     物理画面サイズに依存しない座標をハードコーディングできるようにする。
+        ///     プログラム内では設計画面におけるピクセルの単位として「dpx (designed pixel)」と称することがある。
+        ///     なお、int より float での利用が多いので、Size や Size2 ではなく Size2F を使う。
+        ///     （int 同士ということを忘れて、割り算しておかしくなるケースも多発したので。）
+        /// </remarks>
+        public static SharpDX.Size2F 設計画面サイズ { get; private set; }
+
+        /// <summary>
+        ///     モニタに実際に表示される画面のサイズ[px]。
+        /// </summary>
+        /// <remarks>
+        ///     物理画面サイズは、スワップチェーンのサイズを表す。
+        ///     物理画面サイズは、ユーザが自由に変更することができるため、固定値ではないことに留意。
+        ///     プログラム内では物理画面におけるピクセルの単位として「px (physical pixel)」と称することがある。
+        ///     なお、int より float での利用が多いので、Size や Size2 ではなく Size2F を使う。
+        ///     （int 同士ということを忘れて、割り算しておかしくなるケースも多発したので。）
+        /// </remarks>
+        public static SharpDX.Size2F 物理画面サイズ { get; private set; }
+
+
+
+        // 生成と終了
+
+
+        /// <summary>
+        ///     各種グローバルリソースを生成する。
+        /// </summary>
+        public static void 生成する( AppForm appForm, SharpDX.Size2F 設計画面サイズ, SharpDX.Size2F 物理画面サイズ )
+        {
+            using var _ = new LogBlock( Log.現在のメソッド名 );
+
+            Global.AppForm = appForm;
+            Global.Handle = appForm.Handle;
+
+            Global.設計画面サイズ = 設計画面サイズ;
+            Global.物理画面サイズ = 物理画面サイズ;
+            Log.Info( $"設計画面サイズ: {設計画面サイズ}" );
+            Log.Info( $"物理画面サイズ: {物理画面サイズ}" );
+
+            //Global._スワップチェーンに依存しないグラフィックリソースを作成する();
+            //Global._スワップチェーンを作成する();
+            //Global._スワップチェーンに依存するグラフィックリソースを作成する();
+
+            Global._Dispose済み = false;
+        }
+
+        /// <summary>
+        ///     各種グローバルリソースを開放する。
+        /// </summary>
+        public static void 解放する()
+        {
+            using var _ = new LogBlock( Log.現在のメソッド名 );
+
+            #region " Dispose 済みなら何もしない。"
+            //----------------
+            if( Global._Dispose済み )
+                return;
+
+            Global._Dispose済み = true;
+            //----------------
+            #endregion
+
+            //Global._スワップチェーンに依存するグラフィックリソースを解放する();
+            //Global._スワップチェーンを解放する();
+            //Global._スワップチェーンに依存しないグラフィックリソースを解放する();
+
+            Global.Handle = IntPtr.Zero;
+        }
+
+
+
+        // ローカル
+
+
+        private static bool _Dispose済み = true;
+    }
+}
