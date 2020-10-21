9b011cdfb85dedfd41978a1fd262848bd9f60f3a
FROM / Kenji Yamazaki <kumakamikoubou@gmail.com>
2020-10-21 17:02:58 +0900

    オプション設定画面でパッド矢印が表示されていなかったミスを修正。

--- DTXMania2/ステージ/パッド矢印.cs
+++ DTXMania2/ステージ/パッド矢印.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using SharpDX;
 using SharpDX.Direct2D1;
 using FDK;
@@ -15,7 +13,12 @@ namespace DTXMania2
         // プロパティ
 
 
-        public enum 種類 { 上_Tom1, 下_Tom2, 左_Snare, 右_Tom3 };
+        public enum 種類 { 
+            上_Tom1, 
+            下_Tom2,
+            左_Snare,
+            右_Tom3,
+        };
 
 
 
@@ -26,7 +29,7 @@ namespace DTXMania2
         {
             using var _ = new LogBlock( Log.現在のメソッド名 );
 
-            this._矢印画像 = new 画像D2D( @"$(Images)\PadArraow.png" );
+            this._矢印画像 = new 画像D2D( @"$(Images)\PadArrow.png" );
             this._矢印の矩形リスト = new 矩形リスト( @"$(Images)\PadArrow.yaml" );
         }
 
@@ -42,31 +45,30 @@ namespace DTXMania2
         // 進行と描画
 
 
-        public void 描画する( DeviceContext dc, 種類 type, Vector2 中央位置dpx, float 拡大率 = 1f )
+        public void 描画する( DeviceContext d2ddc, 種類 type, Vector2 中央位置dpx, float 拡大率 = 1f )
         {
-            var 矩形 = new RectangleF?();
-
-            switch( type )
+            var 矩形 = type switch
             {
-                case 種類.上_Tom1: 矩形 = this._矢印の矩形リスト[ "Up" ]; break;
-                case 種類.下_Tom2: 矩形 = this._矢印の矩形リスト[ "Down" ]; break;
-                case 種類.左_Snare: 矩形 = this._矢印の矩形リスト[ "Left" ]; break;
-                case 種類.右_Tom3: 矩形 = this._矢印の矩形リスト[ "Right" ]; break;
-            }
+                種類.上_Tom1 => this._矢印の矩形リスト[ "Up" ],
+                種類.下_Tom2 => this._矢印の矩形リスト[ "Down" ],
+                種類.左_Snare => this._矢印の矩形リスト[ "Left" ],
+                種類.右_Tom3 => this._矢印の矩形リスト[ "Right" ],
+                _ => throw new Exception( "未対応の種類です。" ),
+            };
 
             if( 矩形 is null )
                 return;
 
             var 左上位置dpx = new Vector3(
-                Global.GraphicResources.画面左上dpx.X + 中央位置dpx.X - 矩形.Value.Width * 拡大率 / 2f,
-                Global.GraphicResources.画面左上dpx.Y + 中央位置dpx.Y - 矩形.Value.Height * 拡大率 / 2f,
+                中央位置dpx.X - 矩形.Value.Width * 拡大率 / 2f,
+                中央位置dpx.Y - 矩形.Value.Height * 拡大率 / 2f,
                 0f );
 
             var 変換行列 =
                 Matrix.Scaling( 拡大率 ) *
                 Matrix.Translation( 左上位置dpx );
 
-            this._矢印画像.描画する( dc, 変換行列, 転送元矩形: 矩形 );
+            this._矢印画像.描画する( d2ddc, 変換行列, 転送元矩形: 矩形 );
         }
 
 
