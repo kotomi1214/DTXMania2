117157e8a8c453c74549bb6865aad841fe9661e7
FROM / Kenji Yamazaki <kumakamikoubou@gmail.com>
2020-12-29 13:06:23 +0900

    軽微な修正。

--- DTXMania2/曲/Tree/Node/RandomSelectNode.cs
+++ DTXMania2/曲/Tree/Node/RandomSelectNode.cs
@@ -25,20 +25,21 @@ namespace DTXMania2.曲
         /// </summary>
         public (Score 譜面, SongNode 曲)? 譜面をランダムに選んで返す()
         {
-            for( int retry = 0; retry < 10; retry++ )
+            // RandomSelect がある階層以降すべての SongNode を取得。
+            var songNode配列 = this.親ノード!.Traverse().Where( ( node ) => node is SongNode ).ToArray();
+            int songNode数 = songNode配列.Length;
+
+            if( 0 < songNode数 )
             {
-                // RandomSelect がある階層以降すべての SongNode を取得。
-                var songNode配列 = this.親ノード!.Traverse().Where( ( node ) => node is SongNode ).ToArray();
-                int songNode数 = songNode配列.Count();
+                for( int retry = 0; retry < 10; retry++ )
+                {
+                    // 乱数でノードを決定。
+                    var songNode = songNode配列[ Global.App.乱数.Next( songNode数 ) ] as SongNode;
 
-                if( 0 == songNode数 )
-                    break;
-
-                // 乱数でノードを決定。
-                var songNode = songNode配列[ Global.App.乱数.Next( songNode数 ) ] as SongNode;
-
-                if( null != songNode?.曲.フォーカス譜面 )
-                    return (songNode.曲.フォーカス譜面, songNode);
+                    // 非nullならOK、nullならretry
+                    if( null != songNode?.曲.フォーカス譜面 )
+                        return (songNode.曲.フォーカス譜面, songNode);
+                }
             }
 
             Log.ERROR( $"{nameof( SongNode )} が１つも見つかりません。" );
